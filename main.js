36141b20ea9451c5a7f5c9ea71ad7e0072254366
Indrashis Nath <indrashis.jeet90@gmail.com>
2026-07-26 23:31:00 +0200

    feat: initial static site

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", () => {
+  const message = document.getElementById("message");
+
+  message.textContent = "🎉 Deployed and interactive!";
+});
